07d8a6b76e19bf5cccdcb6f98fa08f0b9cd0446d
enkiv2 <john.ohno@gmail.com>
2015-05-05 07:36:28 -0500

    online help system

--- mycroft.lua
+++ mycroft.lua
@@ -1,5 +1,62 @@
 #!/usr/bin/env lua
 
+version=0.01
+copying=[[Mycroft (c) 2015, John Ohno.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+(License text: BSD-3)
+]]
+help=[[For syntax help, use help("syntax").
+For help with a builtin, use help(builtin/arity).
+To list builtins, use builtins().
+To print the definitions of all defined predicates, use printWorld().\nFor version information, use help("version").
+For information about licensing, use help("copying").]] 
+syntaxHelp=[[Mycroft syntax
+
+Mycroft has a prolog-like syntax, consisting of predicate definitions and queries.
+A predicate definition is of the form:
+	det predicateName(Arg1, Arg2...) :- predicateBody.
+where predicateBody consists of a list of references to other predicates and truth values.
+
+A predicate that is marked 'det' is determinate -- if it ever evaluates to a different value than it has cached, an error is thrown. An indeterminate predicate can be marked 'nondet' instead, meaning that its return value can change and so results from it will not be memoized.
+
+A predicate body is of the form:
+	item1, item2... ; itemA, itemB...
+where each item is either a reference to a predicate or a truth value. Sections separated by ';' are ORed together, while items separated by ',' are ANDed together.
+
+A reference to a predicate is of the form predicateName(Arg1, Arg2...)
+
+A truth value (or composite truth value, or CTV) is of the form
+	<Truth, Confidence>
+where both Truth and Confidence are floating point values between 0 and 1. <X| is syntactic sugar for <X,1.0>; |X> is syntactic sugar for <1.0,X>; YES is syntactic sugar for <1.0, 1.0>; NO is syntactic sugar for <0.0, 1.0>; and, NC is syntactic sugar for <X,0.0> regardless of the value of X.
+
+A query is of the form:
+	?- predicateBody.
+The result of a query will be printed to standard output.
+]]
+
 anonPredCount=0
 
 MYCERR=""
@@ -13,18 +70,39 @@ YES={truth=1, confidence=1}
 NO={truth=0, confidence=1}
 
 builtins={}
-builtins["print/1"]=function(world, c) print(c) return YES end
+helpText={}
+builtins["print/1"]=function(world, c) print(serialize(c)) return YES end
+helpText["print/1"]=[[print(X) will print the value of X to stdout]]
 builtins["printWorld/0"]=function(world) printWorld(world) return YES end
+helpText["printWorld/0"]=[[printWorld() will print the definitions of all defined predicates]]
 builtins["printPred/1"]=function(world, p) if(nil==world[serialize(p)]) then return NO end print(strDef(world, serialize(p))) return YES end
+helpText["printPred/1"]=[[printPred(X) will print the definition of the predicate X to stdout, if X is defined]]
 builtins["throw/1"]=function(world, c) throw(c) return YES end
+helpText["throw/1"]=[[throw(X) will throw the error represented by the error code X
+Available error codes include:
+MYC_ERR_NOERR	]]..tostring(MYC_ERR_NOERR)..[[	no error
+MYC_ERR_UNDEFWORLD	]]..tostring(MYC_ERR_UNDEFWORLD)..[[	world undefined
+MYC_ERR_DETNONDET	]]..tostring(MYC_ERR_DETNONDET)..[[	determinacy conflict: predicate marked det is not determinate, or a predicate marked nondet is having a fact assigned to it]]
 builtins["catch/1"]=function(world, c) if(MYCERR==c) then MYCERR=MYC_ERR_NOERR MYCERR_STR="" return YES end if(MYCERR==MYC_ERR_NOERR) then return YES end return NO end
+helpText["catch/1"]=[[catch(X) will catch the error represented by the error code X and return YES. If there is an error but it is not X, it will return NO.
+Available error codes include:
+MYC_ERR_NOERR	]]..tostring(MYC_ERR_NOERR)..[[	no error
+MYC_ERR_UNDEFWORLD	]]..tostring(MYC_ERR_UNDEFWORLD)..[[	world undefined
+MYC_ERR_DETNONDET	]]..tostring(MYC_ERR_DETNONDET)..[[	determinacy conflict: predicate marked det is not determinate, or a predicate marked nondet is having a fact assigned to it]]
 builtins["exit/0"]=function(world) os.exit() end
+helpText["exit/0"]="exit/0\texit interpreter with no error\nexit(X)\texit with error code X"
 builtins["exit/1"]=function(world, c) os.exit(c) end
+helpText["exit/1"]=helpText["exit/0"]
 builtins["equal/2"]=function(world, a, b) 
 	if(a==b) then return YES end 
 	if(serialize(a)==serialize(b)) then return YES end
 	return NO
 end
+helpText["equal/2"]="equal(X, Y)\treturn YES if X equals Y, otherwise return NO\ngt/2, lt/2, gte/2, and lte/2 work the same way as equal/2, except that values that are neither strings nor numbers are not valid and will return NC"
+helpText["gt/2"]=helpText["equal/2"]
+helpText["lt/2"]=helpText["equal/2"]
+helpText["gte/2"]=helpText["equal/2"]
+helpText["lte/2"]=helpText["equal/2"]
 builtins["gt/2"]=function(world, a, b) 
 	if(type(a)=="table" or type(b)=="table") then return NC end
 	if(a>b) then return YES end
@@ -45,6 +123,23 @@ builtins["lte/2"]=function(world, a, b)
 	if(a<=b) then return YES end
 	return NO
 end
+builtins["builtins/0"]=function(world) for k,v in pairs(builtins) do print(tostring(k)) end return YES end
+helpText["builtins/0"]="builtins/0\tprint all built-in predicates"
+builtins["help/0"]=function(world) print(help) return YES end
+helpText["help/0"]="print general help message"
+helpText["help/1"]="help(X)\tprint help with topic X. See help/0 for details."
+helpText[""]=help
+helpText["copying"]=copying
+helpText["syntax"]=syntaxHelp
+helpText["version"]=tostring(version)
+builtins["help/1"]=function(world,c) 
+	c=serialize(c)
+	if(nil~=helpText[c]) then print(helpText[c])
+	else 
+		print("No help available for "..serialize(c)) 
+		return NO
+	end 
+return YES end
 
 
 function translateArgList(list, conv) -- given an arglist and a conversion map, produce a new arglist with the order transformed
@@ -549,6 +644,8 @@ function testBool()
 end
 function testBuiltins()
 	print("Testing builtins...")
+	print("builtins/0 -> "..serialize(executePredicateNA(world, "builtins", {})))
+	print()
 	print("equal/2(1,1) -> "..serialize(executePredicateNA(world, "equal", {1, 1})))
 	print("equal/2(1,2) -> "..serialize(executePredicateNA(world, "equal", {1, 2})))
 	print("gt/2(1,1) -> "..serialize(executePredicateNA(world, "gt", {1, 1})))
@@ -630,6 +727,17 @@ function testParse()
 	print("Testing parse...")
 	print(parseLine({}, "det true(x, y, z) :- YES."))
 end
+function testHelp()
+	print("Testing online help...")
+	print("help/0 -> "..serialize(executePredicateNA({}, "help", {})))
+	print("help/1(\"syntax\") -> "..serialize(executePredicateNA({}, "help", {"syntax"})))
+	print("help/1(\"version\") -> "..serialize(executePredicateNA({}, "help", {"version"})))
+	print("help/1(\"copying\") -> "..serialize(executePredicateNA({}, "help", {"copying"})))
+	print()
+	for k,v in pairs(builtins) do
+		print("help/1("..serialize(k)..") -> "..serialize(executePredicateNA({}, "help", {k})))
+	end
+end
 function test()
 	testSerialize()
 	testErr()
@@ -637,5 +745,6 @@ function test()
 	testCore()
 	testBuiltins()
 	testParse()
+	testHelp()
 end
 test()
