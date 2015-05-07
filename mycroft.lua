0988d556321cf7ab61d4488766f206a9d70951de
enkiv2 <john.ohno@gmail.com>
2015-05-07 14:34:54 -0500

    support '-e' option for on-command-line expressions; reorganize to make the effects of certain options more sensible

--- mycroft.lua
+++ mycroft.lua
@@ -348,6 +348,7 @@ helpText["banner/0"]=helpText["banner"]
 builtins["banner/0"]=function(world) print(helpText["banner"]) return YES end
 helpText["welcome/0"]=helpText["banner"].."\nType help(). for help, and copying(). for copying information.\n"
 builtins["welcome/0"]=function(world) print(helpText["welcome/0"]) return YES end
+builtins["runtests/0"]=function(world) test() return YES end
 builtins["addpeer/2"]=function(world, address, port) table.insert(mycnet.peers, {address, tonumber(port)}) return YES end
 if(paranoid) then 
 	builtins["setport/1"]=function(world, port) return NO end -- paranoid version
@@ -1142,22 +1143,29 @@ function mainLoop(world)
 end
 
 function main(argv)
-	local world, interactive, i, arg, f
-	setupNetworking()
+	local world, interactive, forceInteractive, testMode, i, arg, f, files, strs
+	files={}
+	strs={}
 	world={}
 	interactive=true
+	testMode=false
 	forceInteractive=false
+	local nextStr=false
 	if(#argv==0) then
 		interactive=true
 	else
 		for i,arg in ipairs(argv) do
-			if("-h"==arg or "-help"==arg or "--help"==arg or "-?"==arg) then
-				print("Usage:\n\tmycroft\nmycroft [-h|-?|--help|-help]\n\tmycroft [file1 file2...] [-i] [-p] [-v]\n\tmycroft -t")
-				print("Options:\n\t-h|-?|-help|--help\t\tPrint this help\n\t-i\t\t\tInteractive mode\n\t-t\t\t\tRun test suite\n\t-p\t\t\tParanoid mode (disable some potentially insecure network-related features\n\t-v\t\t\tVerbose")
-			elseif("-t"==arg) then test() os.exit()
+			if(nextStr) then 
+				table.insert(strs, arg)
+				nextStr=false
+			elseif("-h"==arg or "-help"==arg or "--help"==arg or "-?"==arg) then
+				print("Usage:\n\tmycroft\nmycroft [-h|-?|--help|-help]\n\tmycroft [file1 file2...] [[-e statement ] [-e statement2]...] [-i] [-p] [-v]\n\tmycroft -t")
+				print("Options:\n\t-h|-?|-help|--help\t\tPrint this help\n\t-i\t\t\tInteractive mode\n\t-t\t\t\tRun test suite\n\t-p\t\t\tParanoid mode (disable some potentially insecure network-related features\n\t-v\t\t\tVerbose\n\t-e statement\t\tExecute statement")
+			elseif("-t"==arg) then testMode=true if(not forceInteractive) then interactive=false end
 			elseif("-p"==arg) then paranoid=true
 			elseif("-v"==arg) then verbose=true
 			elseif("-i"==arg) then interactive=true forceInteractive=true
+			elseif("-e"==arg) then nextStr=true if(not forceInteractive) then interactive=false end
 			else 
 				if(not forceInteractive) then interactive=false end
 				f, err=io.open(arg)
@@ -1165,9 +1173,19 @@ function main(argv)
 					print("Could not open file "..arg.." for reading: "..tostring(err).."\nTry mycroft -h for help")
 					os.exit(1)
 				end
-				parseFile(world, f)
+				table.insert(files, f)
 			end
 		end
+	end
+	setupNetworking()
+	for _,f in ipairs(files) do
+		parseFile(world, f)
+	end
+	for _,f in ipairs(strs) do
+		parseLine(world, f)
+	end
+	if(testMode) then
+		test()
 	end
 	if(interactive) then
 		print(serialize(executePredicateNA(world, "welcome", {})))
