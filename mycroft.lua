5e2386b245a15018dd10bd3931601f0480e452ab
enkiv2 <john.ohno@gmail.com>
2015-05-07 14:17:27 -0500

    start on ffi support for fallback if luasocket isn't installed

--- mycroft.lua
+++ mycroft.lua
@@ -366,7 +366,35 @@ end
 function setupNetworkingLJIT()
 	debugPrint("detected luajit; setting up.")
 	setupNetworkingCommon()
-	setupNetworkingDummy() --XXX
+	if(pcall(require, "socket")) then
+		setupNetworkingLSOCK() -- prefer LuaSocket
+	elseif(pcall(require, "ffi")) then
+		ffi=require("ffi")
+		ffi.cdef[[
+			int     accept(int, struct sockaddr *restrict, socklen_t *restrict);
+			int     bind(int, const struct sockaddr *, socklen_t);
+			int     connect(int, const struct sockaddr *, socklen_t);
+			int     getpeername(int, struct sockaddr *restrict, socklen_t *restrict);
+			int     getsockname(int, struct sockaddr *restrict, socklen_t *restrict);
+			int     getsockopt(int, int, int, void *restrict, socklen_t *restrict);
+			int     listen(int, int);
+			ssize_t recv(int, void *, size_t, int);
+			ssize_t recvfrom(int, void *restrict, size_t, int, struct sockaddr *restrict, socklen_t *restrict);
+			ssize_t recvmsg(int, struct msghdr *, int);
+			ssize_t send(int, const void *, size_t, int);
+			ssize_t sendmsg(int, const struct msghdr *, int);
+			ssize_t sendto(int, const void *, size_t, int, const struct sockaddr *, socklen_t);
+			int     setsockopt(int, int, int, const void *, socklen_t);
+			int     shutdown(int, int);
+			int     socket(int, int, int);
+			int     sockatmark(int);
+			int     socketpair(int, int, int, int[2]);
+
+		]]
+	else
+		debugPrint("Despite having the jit table, we are not luajit or are unable to load ffi. Falling back to luasock")
+		setupNetworkingLSOCK()
+	end
 end
 function setupNetworkingLSOCK()
 	debugPrint("detected luasocket; setting up.")
