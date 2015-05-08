649df1f1f464ba420b026b3b6047826b2daa996f
enkiv2 <john.ohno@gmail.com>
2015-05-08 16:41:19 -0500

    fixes

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --[[ Mycroft language
 ]]--
 for _,module in ipairs({"mycBuiltins","mycCore","mycErr","mycNet","mycParse","mycPretty","mycTests","mycType"}) do
-	package.predload[module]=function() return require("mycroft."..module) end
+	package.preload[module]=function() return require("mycroft."..module) end
 end
 for _,module in ipairs({"mycBuiltins","mycCore","mycErr","mycNet","mycParse","mycPretty","mycTests","mycType"}) do
 	require("mycroft."..module)
