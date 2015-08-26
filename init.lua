f5465ae223bd0caa4ae6fbd6fc403cf923daafc7
enkiv2 <john.ohno@gmail.com>
2015-08-26 13:07:42 -0500

    need to preload hash.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 --[[ Mycroft language
 ]]--
-for _,module in ipairs({"mycBuiltins","mycCore","mycErr","mycNet","mycParse","mycPretty","mycTests","mycType"}) do
+for _,module in ipairs({"mycBuiltins","mycCore","mycErr","mycNet","mycParse","mycPretty","mycTests","mycType", "hash"}) do
 	package.preload[module]=function() return require("mycroft."..module) end
 end
-for _,module in ipairs({"mycBuiltins","mycCore","mycErr","mycNet","mycParse","mycPretty","mycTests","mycType"}) do
+for _,module in ipairs({"mycBuiltins","mycCore","mycErr","mycNet","mycParse","mycPretty","mycTests","mycType", "hash"}) do
 	require("mycroft."..module)
 end
 
